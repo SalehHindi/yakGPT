501b10cfd9f928bcd425bad4bf55e1cfbc63c285
Saleh Hindi <saleh.hindi.one@gmail.com>
2023-04-17 09:43:08 -0400

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home() {
         <meta name="description" content="A new way to do work" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        
       </Head>
       <AppShell
         padding={0}
